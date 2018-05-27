dfa4cffbaaed695485bb8627ff806e41db87bad5
IvanMadrigalPerezISC <imadrigal@bcodemexico.com>
2018-05-26 23:41:07 -0500

    estructura de datos regresa lectura de lista enlazada simple

--- Basta/app/src/main/cpp/native-lib.cpp
+++ Basta/app/src/main/cpp/native-lib.cpp
@@ -7,23 +7,6 @@
 
 extern "C"
 
-JNIEXPORT jint JNICALL
-Java_com_example_android_basta_MainActivity_leerpuntaje(JNIEnv *env, jobject instance,
-                                                        jintArray puntaje_) {
-    jint *puntaje = env->GetIntArrayElements(puntaje_, NULL);
-    jint *puntajetraido = env->GetIntArrayElements(puntaje_, NULL);
-    int contadorpuntos=0;
-    std::string resultado ="";
-    // TODO
-    for(int i=0;i<5;i++){
-        contadorpuntos=puntaje[i]+contadorpuntos;
-    }
-    //resultado=contadorpuntos;
-    env->ReleaseIntArrayElements(puntaje_, puntaje, 0);
-    return contadorpuntos;
-}
-
-
 typedef struct ElementoLista {
     int *dato;
     struct ElementoLista *siguiente;
@@ -75,11 +58,43 @@ int ins_inicio_lista (Lista * lista, int *dato){
 }
 
 /* visualización de la lista */
-void visualización (Lista * lista){
+int visualizacion (Lista * lista){
     Elemento *actual;
+    int puntajetotal=0;
     actual = lista->inicio;
     while (actual != NULL){
-        printf ("%p - %s\n", actual, actual->dato);
+        puntajetotal= reinterpret_cast<int>(actual->dato + puntajetotal);
+        //printf ("%p - %s\n", actual, actual->dato);
         actual = actual->siguiente;
     }
+    return puntajetotal;
 }
+
+
+
+JNIEXPORT jint JNICALL
+Java_com_example_android_basta_MainActivity_leerpuntaje(JNIEnv *env, jobject instance,
+                                                        jintArray puntaje_) {
+    jint *puntaje = env->GetIntArrayElements(puntaje_, NULL);
+    jint *puntajetraido = env->GetIntArrayElements(puntaje_, NULL);
+    int contadorpuntos=0;
+    //std::string resultado ="";
+    // TODO
+    /*
+    for(int i=0;i<5;i++){
+        contadorpuntos=puntaje[i]+contadorpuntos;
+    }*/
+    //resultado=contadorpuntos;
+    Lista *lista;
+    inicializacion(lista);
+    ins_en_lista_vacia (lista, reinterpret_cast<int *>(puntaje[0]));
+    for(int i=1;i<5;i++){
+        ins_inicio_lista (lista, reinterpret_cast<int *>(puntaje[i]));
+        //contadorpuntos=puntaje[i]+contadorpuntos;
+    }
+    //ins_en_lista_vacia (Lista * lista, int *dato);
+    env->ReleaseIntArrayElements(puntaje_, puntaje, 0);
+    return visualizacion(lista);
+}
+
+
