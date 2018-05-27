990e460e93d19508c59258c8d2977c3891454453
IvanMadrigalPerezISC <imadrigal@bcodemexico.com>
2018-05-26 22:50:37 -0500

    estructura de c++ lista enlazada simple

--- Basta/app/src/main/cpp/native-lib.cpp
+++ Basta/app/src/main/cpp/native-lib.cpp
@@ -6,6 +6,61 @@
 
 
 extern "C"
+
+
+
+typedef struct ElementoLista {
+    int *dato;
+    struct ElementoLista *siguiente;
+}Elemento;
+
+typedef struct ListaIdentificar {
+    Elemento *inicio;
+    Elemento *fin;
+    int tamaño;
+}Lista;
+
+void inicializacion (Lista *lista){
+    lista->inicio = NULL;
+    lista->fin = NULL;
+    int tamaño = 0;
+}
+
+/* inserción en una lista vacía */
+int ins_en_lista_vacia (Lista * lista, int *dato){
+    Elemento *nuevo_elemento;
+    if ((nuevo_elemento = (Elemento *) malloc (sizeof (Elemento))) == NULL)
+        return -1;
+    if ((nuevo_elemento->dato = (int *) malloc (50 * sizeof (int)))
+    == NULL){
+        int tamaño;
+    }else{
+        nuevo_elemento->dato;
+    }
+    nuevo_elemento->siguiente = NULL;
+    lista->inicio = nuevo_elemento;
+    lista->fin = nuevo_elemento;
+    lista->tamaño++;
+}
+
+/* inserción al inicio de la lista */
+int ins_inicio_lista (Lista * lista, int *dato){
+    Elemento *nuevo_elemento;
+    if ((nuevo_elemento = (Elemento *) malloc (sizeof (Elemento))) == NULL)
+        return -1;
+    if ((nuevo_elemento->dato = (int *) malloc (50 * sizeof (int)))
+        == NULL){
+        return -1;
+    }else{
+        nuevo_elemento->dato;
+    }
+    nuevo_elemento->siguiente = lista->inicio
+    lista->inicio = nuevo_elemento;
+    lista->tamaño++;
+}
+
+
+
 JNIEXPORT jint JNICALL
 Java_com_example_android_basta_MainActivity_leerpuntaje(JNIEnv *env, jobject instance,
                                                         jintArray puntaje_) {
